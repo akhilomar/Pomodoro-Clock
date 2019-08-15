9593eef1bab86435487062c80bf8816f1afc9635
akhilomar <akhilomar555@gmail.com>
2019-08-16 03:39:30 +0530

    Updates

---
+++ precache-manifest.4828581d997ef22adf14b6e8a23524ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c57efa52b0e0919209082ca7e7105e",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "0c7f2789a13f259737b7",
+    "url": "/Pomodoro-Clock/static/css/main.b6fce2b9.chunk.css"
+  },
+  {
+    "revision": "4a9553a6bb99e93be759",
+    "url": "/Pomodoro-Clock/static/js/2.b9f8b894.chunk.js"
+  },
+  {
+    "revision": "0c7f2789a13f259737b7",
+    "url": "/Pomodoro-Clock/static/js/main.67a00d4f.chunk.js"
+  },
+  {
+    "revision": "3fa90cbd360ae40ebf83",
+    "url": "/Pomodoro-Clock/static/js/runtime~main.81c896c1.js"
+  }
+]);
